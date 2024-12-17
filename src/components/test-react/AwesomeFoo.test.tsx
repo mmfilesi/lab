41a18122eb617c3b759650f84530c180f2226427
Marcos MMFI. Mendez Filesi <marcos.mendez@softtekprojects.com>
2024-12-17 13:40:00 +0100

    wp: tests pipeline

--- src/components/test-react/AwesomeFoo.test.tsx
+++ src/components/test-react/AwesomeFoo.test.tsx
@@ -1,4 +1,4 @@
-import { render  } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 
 import AwesomeFoo from './AwesomeFoo';
@@ -6,7 +6,7 @@ import AwesomeFoo from './AwesomeFoo';
 describe('AwesomeFoo Component', () => {
   it('renders the correct content', () => {
     // Renderiza el componente
-    render(<AwesomeFoo />)
+    render(<AwesomeFoo />);
 
     // Selecciona el elemento usando el atributo 'data-qa'
     const element = document.querySelector('[data-qa="awesome-foo"]');
@@ -15,5 +15,4 @@ describe('AwesomeFoo Component', () => {
     expect(element).toBeInTheDocument();
     // expect(element).toHaveTextContent("I'm an awesome component that says Foo!");
   });
-
-})
+});
